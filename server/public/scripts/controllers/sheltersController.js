9329e50389558e0eae1c1c072bb6b5c0664efea4
atomassoni <annetomassoni@gmail.com>
2016-05-26 13:39:55 -0500

    added pet names on shelter click

--- server/public/scripts/controllers/sheltersController.js
+++ server/public/scripts/controllers/sheltersController.js
@@ -21,4 +21,26 @@ myApp.controller('SheltersController', ['$scope', '$http', function ($scope,  $h
       }
     )
   }
+
+  $scope.getPetsFromShelter = function(id) {
+    var key = '5433d627c0c62b99a9af9fbbe4227a02';
+    var baseURL = 'http://api.petfinder.com/';
+
+
+    var query = 'shelter.getPets';
+    query += '?key=' + key;
+    query += '&id=' + id;
+    query += '&format=json';
+
+    var request = baseURL + encodeURI(query) + '&callback=JSON_CALLBACK';
+
+    console.log(request);
+
+    $http.jsonp(request).then(
+      function(response) {
+        console.log('pets: ', response.data);
+        $scope.pets = response.data.petfinder.pets.pet;
+      }
+    )
+  }
 }]);
